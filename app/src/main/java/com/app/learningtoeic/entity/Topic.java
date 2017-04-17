5e1ac07ee89e476c527b772bf2d62abe3b374e70
QUYET <kienquyetit@gmail.com>
2017-04-17 18:34:24 +0700

    17/4 Thay đổi DBHelper

--- app/src/main/java/com/app/learningtoeic/entity/Topic.java
+++ app/src/main/java/com/app/learningtoeic/entity/Topic.java
@@ -1,8 +1,5 @@
 package com.app.learningtoeic.entity;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by dell on 4/11/2017.
  */
--- app/src/main/java/com/app/learningtoeic/presenter/DictionaryPresenter.java
+++ app/src/main/java/com/app/learningtoeic/presenter/DictionaryPresenter.java
@@ -1,31 +1,12 @@
 package com.app.learningtoeic.presenter;
 
-import android.app.ProgressDialog;
-import android.content.res.AssetManager;
 import android.os.AsyncTask;
-import android.support.v7.widget.LinearLayoutManager;
-import android.view.View;
-import android.widget.ArrayAdapter;
 
-import com.app.learningtoeic.R;
 import com.app.learningtoeic.contract.DictionaryContract;
 import com.app.learningtoeic.entity.Word;
 import com.app.learningtoeic.mvp.fragment.FragmentPresenter;
 import com.app.learningtoeic.utils.Config;
-import com.app.learningtoeic.utils.ProgressDialogHelper;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.util.ArrayList;
 
 /**
